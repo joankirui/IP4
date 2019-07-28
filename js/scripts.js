fae2a3edd213e9d5b09e7a38dfab01ddcbee626d
joankirui <joankirui99@gmail.com>
2019-07-28 14:31:02 +0300

    add a constructor called order that shows the total charge for the orders

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function order(size,crust,toppings,total){
+    this.pizzaSize = size;
+    this.pizzaCrust = crust;
+    this.pizzaToppings = toppings;
+    this.total = total;
+}
+$(document).ready(function()
